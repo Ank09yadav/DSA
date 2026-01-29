e329387fbd0cddd7095670bf5c20c2db30486095
Ank09yadav <ankury114112118@gmail.com>
2026-01-29 12:24:38 +0530

    jagged array in cpp

---
+++ jaggedArray.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int columnSize[]={2,3,6,2,};
+    int* jagged[4];
+
+    for(int i=0;i<4;i++)
+        jagged[i] = new int[columnSize[i]];
+    
+    cout<<"Enter a number ";
+    for(int i=0; i<4;i++){
+        for(int j=0;j<columnSize[i];j++ ){
+           cin>>jagged[i][j];
+        }
+    }
+
+    for(int i=0; i<4;i++){
+        for(int j=0;j<columnSize[i];j++ ){
+            cout<<jagged[i]<<endl;
+        }
+    }
+
+    cout<<"Element of the jagged Array :";
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < columnSize[i]; j++) {
+            cout << jagged[i][j] << " ";
+        }
+        cout << endl;
+    }
+    //Free memory 
+    for(int i = 0; i < 4; i++) {
+        delete[] jagged[i];
+    }
+    return 0;
+
+}
